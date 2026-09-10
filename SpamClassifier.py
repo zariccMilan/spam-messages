43fa1accb4c597ab6b362a9dcd19585a22c3fd7b
Milan Zaric <zaric.milan11@gmail.com>
2026-09-10 05:55:02 +0200

    Added explanation for every step

--- SpamClassifier.py
+++ SpamClassifier.py
@@ -25,20 +25,40 @@ df.columns = ['label', 'message']
 df['label'] = df['label'].map({'ham': 0, 'spam': 1})
 
 # 2. Share data
+# x is for entry data and y is for right answers 0 or 1
+# train is what model use for studying and test in this step model does not use
+# 20 % is for test and 80% for training
+# 42 is just whole number and it's for repeated results to see same numbers, and model will always start from 42.
 X_train, X_test, y_train, y_test = train_test_split(
     df['message'], df['label'], test_size=0.2, random_state=42
 )
 
 # 3. Transfer text to numbers
+# TF = Term Frequency, mesures how many times one word is repeated in one message
+# IDF = Inverse Document Frequency, lowers rating of words that are common in all messages
+# Word that repeat more in one message and not in other messages are higher rated
+# Words like 'win' or 'iphone' repeats many times and that helps for spam messages but also common english words, that's why stop_words='english' is added
 vectorizer = TfidfVectorizer(stop_words='english')
+# fit is telling TF-IDF to learn words and rates but only from training
+# transform is converting from text messages to numeric vectors
 X_train_vec = vectorizer.fit_transform(X_train)
+# test data converts with already learned dictionary without fit because model would than access to test data and evaluation would be compromised
 X_test_vec = vectorizer.transform(X_test)
 
 # 4. Train model
+# Multinomial Naive Bayes model
+# this model is for classification based on statistics
+# how does it work? checks what are common words in spam and ham messages and what class in more dominant than it chooses the class with more estimated probability
+# why naive ? because it simplified estimates that words don't have any connections or relationship, this is not true but model still works good
 model = MultinomialNB()
+# sms messages that are converted in numbers
+# y_train are their real values; 'ham' or 'spam'
+# this is learning time
 model.fit(X_train_vec, y_train)
 
 # 5. Test
+# prediction, this is where model gets messages that weren't in training and gives for everyone prediction
+# y_pred are predictions and y_test are real values from dataset
 y_pred = model.predict(X_test_vec)
 print(f"Tačnost: {accuracy_score(y_test, y_pred):.2%}")
 print(classification_report(y_test, y_pred))
